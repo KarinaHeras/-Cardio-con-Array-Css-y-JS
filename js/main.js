51470394eac0e71d05d7e26d82206a839ca1bf17
KarinaHeras <karinaheras1847@gmail.com>
2019-03-26 13:34:01 +0100

    aqui seguimos con map, reduce, filter ... luego seguimos

--- js/main.js
+++ js/main.js
@@ -16,5 +16,60 @@ const people = ['Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick
   'Berlin , Iirving', 'Berlne , Eric', 'Bernhard, Sandra', 'Berra , yogi', 'Berry , Halle',
   'Berry , Wendell', 'Betha , Erin ', 'Bevan , Aneurin', 'Bavel , ken', 'Piden , Joseph' , 
   'Bierce , Ambrose', 'Biko , Steve', 'Billings , Josh', 'Biondo , Frank', 'Birreli , Augustine',
-  'Black Elk', 'Blair , Robert', 'Blair , Robert', 'Blair , Tony', 'Blake , William'];                     ]
+  'Black Elk', 'Blair , Robert', 'Blair , Robert', 'Blair , Tony', 'Blake , William'];                     
+
+//array.prototype.filter()
+// 1 filter the fist of inventors for those who were bour in the 1500's
+
+const fifteen = inventors.filter(inventor => (inventor.year >= 1500 && inventor.year < 1600 ))
+console.table(fifteen);
+
+//Array.prototype.map() 
+// 2. Give us an array of the inentory first and last names
+
+const fullName = inventors.map(inventor => `${inventor.first} ${inventor.last}`);
+console.log(fullName);
+
+//Array.prototype.sort()
+// 3 sort the inventor by birthdate, oldest to yurgest
+
+// const ordered = inventors.sort(funtion (a,b){
+//  if(a.year > b.year){
+//        return 1;
+
+ //   } else {
+//        return -1;
+//    }
+//});
+
+const ordered = inventors.sort((a, b) => a.year > b.year ? 1 : -1);
+console.table(ordered)
+
+// Array.prototype.reduce()
+// 4. How many year did all the inventors live?
+const totalYear = inventors.reduce((total, inventor) => {
+    return total + (inventor.passed - inventor.year);
+}, 0);
+console.log(totalYear);
+
+// 5. sort the inventor by years lived
+
+// 6. Create a list of boulevard in paris that contain 'de' anywhere in the name
+// https://en.wikipedia.org/wiki/category:Boulevards_in_paris 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
